55cca73550cebc6dca75b3c005f84ce75213e964
Akshaysingh6394 <singhakshay8081@gmail.com>
2025-02-19 09:49:42 +0530

    PATTERN

---
+++ Pattern/test.java
@@ -0,0 +1,12 @@
+package Pattern;
+
+public class test {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i <= n; i++) {
+            System.out.println(i);
+            
+        }
+    }
+    
+}
