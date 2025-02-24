ed045f535232b27404e6efde187d6f09bb839e7f
Akshaysingh6394 <singhakshay8081@gmail.com>
2025-02-24 14:44:00 +0530

    remove duplicate from sorted array

--- Array/removeduplicate.java
+++ Array/removeduplicate.java
@@ -1,23 +1,24 @@
 
 public class removeduplicate {
-    public static void main(String[] args) {
-       int arr[] = {1,1,2,2,2,3,3, 5,5,8,8,8};
-        int k = removeDuplicates(arr);
-        System.out.println("The array after removing duplicate elements is ");
-        for (int i = 0; i < k; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-    static int removeDuplicates(int[] arr) {
-        int i = 0;
+    public static int removedipl(int arr[]){
+        int i =0;
         for (int j = 1; j < arr.length; j++) {
-            if (arr[i] != arr[j]) {
-                
+            if(arr[i] != arr[j]){
                 i++;
                 arr[i] = arr[j];
             }
+            
+            
         }
-        return i + 1;
+        return i+1;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,1,1,2,3,3,3,4,4,7,9,9,9,9};
+        int k = removedipl(arr);
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[i]+" ");
+        }
+       
     }
 }
 
