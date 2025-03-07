cc7c58091d60008c567daf6018495012544a5893
Akshaysingh6394 <singhakshay8081@gmail.com>
2025-03-07 11:55:34 +0530

    new commit

--- 2DArray/large.java
+++ 2DArray/large.java
@@ -2,11 +2,10 @@ public class large{
     public static void main(String[] args) {
         int numbr[][] = {{1,2,3},{4,9,6},{7,8,0}};
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < numbr.length; i++) {
+        for (int[] numbr1 : numbr) {
             for (int j = 0; j < numbr.length; j++) {
-                if(numbr[i][j]>max){
-                    max=numbr[i][j];
-
+                if (numbr1[j] > max) {
+                    max = numbr1[j];
                 }
             }
         }
