838c32d42af5772199bc6cd43dbdceaf425df9b2
Akshaysingh6394 <singhakshay8081@gmail.com>
2025-03-10 15:15:22 +0530

    sorting with method

---
+++ 2DArray/zerones.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class zerones {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,3,3,2,2,1,1,1,1};
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+
+
+    }
+    
+}
