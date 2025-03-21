22c6aa5f4269518d26652bc12a5dcc488f594ca8
Akshaysingh6394 <singhakshay8081@gmail.com>
2025-03-21 23:27:08 +0530

    check if array is sorted

---
+++ Recursion/checksort.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class checksort {
+    public static boolean checkSorted(int arr[],int i){
+        if(i==arr.length-1){
+            return true;
+        }
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+        return checkSorted(arr, i+1);
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,6,5,6};
+        int i=0;
+        System.out.println(checkSorted(arr, i));
+    }
+    
+}
