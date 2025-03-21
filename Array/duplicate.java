a3527a8747b4b03464ddc08db1ccfc12cf471077
Akshaysingh6394 <singhakshay8081@gmail.com>
2025-03-22 00:58:36 +0530

    remove duplicate in array

--- Array/duplicate.java
+++ Array/duplicate.java
@@ -1,8 +1,9 @@
+
+
 public class duplicate {
-    public static void dupli(int arr[] , int n){
-        
-    }
-    public static int main(String[] args) {
+    // 
+    
+    public static void main(String[] args) {
         int arr[] = {1,2,3,3,3,4,4,4,5,5,5,7};
         int i = 0;
         for (int j = 1; j < arr.length; j++) {
@@ -10,9 +11,16 @@ public class duplicate {
                 i++;
                 arr[i] = arr[j];
             }
-            System.out.println(i+1);
+            // System.out.println(i+1);
             
         }
+        int k = i+1;
+       for (int j = 0; j < k; j++) {
+        System.out.println(arr[j]);
+        
+       }
+
+        
     }
     
 }
