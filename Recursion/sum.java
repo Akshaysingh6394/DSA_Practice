45de4b7b07d6440dbbd09eecbdd6dbd05bfccccd
Akshaysingh6394 <singhakshay8081@gmail.com>
2025-03-24 09:46:45 +0530

    recursion sum other method

--- Recursion/sum.java
+++ Recursion/sum.java
@@ -1,17 +1,28 @@
 package Recursion;
 
 public class sum {
-    public static int sumOfNaturalNumbers(int n){
-        if(n==1){
-            // System.out.println(1);
-            return 1;
+    // public static int sumOfNaturalNumbers(int n){
+    //     if(n==1){
+    //         // System.out.println(1);
+    //         return 1;
+    //     }
+    //     int smn =n+sumOfNaturalNumbers(n-1);
+    //     return smn;
+    // }
+    // public static void main(String[] args) {
+    //     int n =5;
+    //     System.out.println(sumOfNaturalNumbers(n));
+    // }
+    public static void sumof(int n,int sum){
+        if(n<1){
+            System.out.println(sum);
+            return;
         }
-        int smn =n+sumOfNaturalNumbers(n-1);
-        return smn;
+        sumof(n-1, sum+n);
     }
     public static void main(String[] args) {
-        int n =5;
-        System.out.println(sumOfNaturalNumbers(n));
+      
+        sumof(5, 0);
     }
     
 }
