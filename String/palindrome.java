f948bf965ca0e04d66cb06033708bf5ea3b87760
Akshaysingh6394 <singhakshay8081@gmail.com>
2025-03-26 16:09:22 +0530

    palindrome

--- String/palindrome.java
+++ String/palindrome.java
@@ -2,6 +2,7 @@ package String;
 
 public class palindrome {
     public static boolean checkPalind(String str){
+        str = str.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
         for (int i = 0; i < str.length()/2; i++) {
             if(str.charAt(i)!=str.charAt(str.length()-i-1)){
                 return false;
@@ -13,7 +14,7 @@ public class palindrome {
     }
     
     public static void main(String[] args) {
-        String str = "noon";
+        String str = "A man, a plan, a canal: Panama";
         System.out.println(checkPalind(str));
     }
     
