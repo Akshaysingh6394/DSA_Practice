a2290ea2d828de921c191b058e15bfd0604a2efa
Akshaysingh6394 <singhakshay8081@gmail.com>
2025-03-27 02:39:46 +0530

    linear search using recursion

--- Recursion/linear.java
+++ Recursion/linear.java
@@ -1,16 +1,17 @@
 package Recursion;
 
 public class linear {
-    public static int linearSearch(int arr[],int target,int i){
+    public static void linearSearch(int arr[],int target,int i){
         if(arr[i]==target){
-            return i;
+            System.out.println(i);
+            return ;
         }
-        return linearSearch(arr, target, i+1);
+        linearSearch(arr, target, i+1);
     }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,6,8,0};
         int target = 8;
-        System.out.println(linearSearch(arr, target, 0));
+        linearSearch(arr, target, 0);
         
 
     }
