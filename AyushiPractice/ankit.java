62d283770dab3fb6297e44931b36bf7cea7f899f
Akshaysingh6394 <singhakshay8081@gmail.com>
2025-03-31 03:47:22 +0530

    practise with ankit

---
+++ AyushiPractice/ankit.java
@@ -0,0 +1,14 @@
+package AyushiPractice;
+
+public class ankit {
+    public static void main(String[] args) {
+        // for (int i = 0; i < 6; i++) {
+        //     System.out.println(i);
+        // }
+        // int a=5;
+        System.out.println(a++);
+        System.out.println(a);
+      
+    }
+    
+}
--- String/labled.java
+++ String/labled.java
@@ -1,13 +1,25 @@
 package String;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class labled {
-    public List<Integer> partitionLabels(String s) {
-        for (int i = 0; i < str; i++) {
+    public List<Integer> partitionLabels(String str) {
+        List<Integer> partitions = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            int startIndex = i;
+            int endIndex = str.lastIndexOf(str.charAt(startIndex));
+            for(int s=startIndex+1;s<=endIndex-1;s++){
+                int lastIndexOfNextChar = str.lastIndexOf(str.charAt(s));
+                if(lastIndexOfNextChar>endIndex){
+                    endIndex = lastIndexOfNextChar;
+                }
+            }
+            partitions.add(endIndex-startIndex+1);
+            i=endIndex+1;
             
         }
+        return partitions;
     }
     public static void main(String[] args) {
         
