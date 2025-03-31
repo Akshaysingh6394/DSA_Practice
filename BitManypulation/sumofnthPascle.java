5677be24801b64217a9028c0f78e81025400bb2e
Akshaysingh6394 <singhakshay8081@gmail.com>
2025-04-01 01:54:12 +0530

    find the sum of nth row in Pascle triangle

---
+++ BitManypulation/sumofnthPascle.java
@@ -0,0 +1,11 @@
+package BitManypulation;
+
+public class sumofnthPascle {
+    public static void main(String[] args) {
+        int n=4;
+        int ans = 1<<(n-1);
+        // ans += 1<<(n-1);
+        System.out.println(ans);
+    }
+    
+}
