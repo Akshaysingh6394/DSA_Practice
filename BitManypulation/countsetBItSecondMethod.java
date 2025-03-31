7077e584144bf5ebf14a519b91c6c8e67e88f78d
Akshaysingh6394 <singhakshay8081@gmail.com>
2025-04-01 02:23:22 +0530

    second method of count set bit

---
+++ BitManypulation/countsetBItSecondMethod.java
@@ -0,0 +1,16 @@
+package BitManypulation;
+
+public class countsetBItSecondMethod {
+    public static void main(String[] args) {
+        int n= 10;
+        System.out.println(Integer.toBinaryString(n));
+        int count = 0;
+        while(n>0){
+            count++;
+            n -= (n & -n);
+            
+        }
+        System.out.println(count);
+    }
+    
+}
