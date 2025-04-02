1251686158e4cc19c327b63211fbf4f0f424c253
Akshaysingh6394 <singhakshay8081@gmail.com>
2025-04-02 10:45:48 +0530

    sub string using recursion

---
+++ adRecursion/subString.java
@@ -0,0 +1,17 @@
+package adRecursion;
+
+public class subString {
+    public static void subSe(String p , String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subSe(p+ch, up.substring(1));
+        subSe(p, up.substring(1));
+    }
+    public static void main(String[] args) {
+        subSe(" ", "abc");
+    }
+    
+}
