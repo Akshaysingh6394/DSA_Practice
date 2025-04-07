07c770c8f5da591ad434d269841ee3dcab4909f4
Akshaysingh6394 <singhakshay8081@gmail.com>
2025-04-07 11:00:15 +0530

    LinkedList concepts

--- AyushiPractice/ankit.java
+++ AyushiPractice/ankit.java
@@ -1,14 +1,25 @@
 package AyushiPractice;
 
 public class ankit {
-    public static void main(String[] args) {
-        // for (int i = 0; i < 6; i++) {
-        //     System.out.println(i);
+    public static void Rev(int n){
+        // int rev = 0;
+        // while(n>0){
+        //     int rem = n%10;
+        //     rev = rev*10+rem;
+        //     n/=10;
         // }
-        // int a=5;
-        // System.out.println(a++);
-        // System.out.println(a);
-        System.out.println(4^0);
+        // System.out.println(rev);
+        if(n==0){
+
+            return;
+        }
+        int rem = n%10;
+        System.out.print(rem);
+        Rev(n/10);
+    }
+    public static void main(String[] args) {
+        int n = 123;
+        Rev(n);
       
     }
     
--- LinkedList/concept.java
+++ LinkedList/concept.java
@@ -13,12 +13,39 @@ class Node{
 }
 public static Node head;
 public static Node tail;
-public static void addFirst(int data){
+public void addFirst(int data){
     Node newNode = new Node(data);
-    if(head=null)
+    if(head==null){
+        head = tail = newNode;
+        return;
+    }
+    newNode.next = head;
+    head = newNode;
+}
+public void addLast(int data){
+    Node newNode = new Node(data);
+    if(head==null){
+        head=tail =null;
+        return;
+    }
+    tail.next = newNode;
+    tail=newNode;
+}
+public void print(){
+    Node temp = head;
+    while(temp!=null){
+        System.out.print(temp.data+"------->");
+        temp = temp.next;
+    }
+    System.out.println("null");
 }
     
     public static void main(String[] args) {    
     concept ll = new concept();
+    ll.addFirst(2);
+    ll.addFirst(1);
+    ll.addLast(3);
+    ll.addLast(4);
+    ll.print();
 }
 }
