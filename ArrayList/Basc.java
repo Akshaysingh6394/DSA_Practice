f351226537796110dcfcefa598e8025f29988d4e
Akshaysingh6394 <singhakshay8081@gmail.com>
2025-04-17 15:03:14 +0530

    basic concept of arraylist

--- ArrayList/Basc.java
+++ ArrayList/Basc.java
@@ -1,15 +1,18 @@
 package ArrayList;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Basc {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 10; i > 0; i--) {
             list.add(i);
         }
         System.out.println(list);
-
+        System.out.println(list.contains(6));
+        Collections.sort(list);
+        System.out.println(list);
     }
     
 }
