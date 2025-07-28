51cb6620ec6c516d7dc111539bb75bc0fed041c3
Akshay Singh <singhakshay8081@gmail.com>
2025-07-28 18:36:48 +0530

    Binary Tree

--- Stack/addRemove.java
+++ Stack/addRemove.java
@@ -3,14 +3,18 @@ public class addRemove {
     public static void main(String[] args) {
         
         Stack<Integer> s = new Stack<>();
-        int n = s.size();
+        int size = 0;
         s.push(1);
         s.push(2);
         s.push(3);
         s.push(4);
+System.out.println(s.size());
         while(!s.isEmpty()){
             System.out.print(s.pop());
+            size++;
         }
+        System.out.println();
+
         
     }
     
